9b17eb9d7ce146eeacdf19d33a4f350f885891d8
Aluno <Aluno@SN-279266>
2019-02-27 20:40:29 -0300

    AULA2702

--- src/br/senai/sc/sisloj/SisLojFrame.java
+++ src/br/senai/sc/sisloj/SisLojFrame.java
@@ -5,6 +5,10 @@
  */
 package br.senai.sc.sisloj;
 
+import br.senai.sc.sisloj.views.CadastroCliente;
+import br.senai.sc.sisloj.views.PainelAmarelo;
+import br.senai.sc.sisloj.views.PainelRosa;
+import java.awt.CardLayout;
 import javax.swing.JOptionPane;
 
 /**
@@ -17,7 +21,18 @@ public class SisLojFrame extends javax.swing.JFrame {
      * Creates new form SisLojFrame
      */
     public SisLojFrame() {
-        initComponents();
+        initComponents();//metodo construtor
+        
+        PainelRosa pRosa = new PainelRosa();
+        PainelAmarelo pAmarelo = new PainelAmarelo();
+        CadastroCliente cadCli = new CadastroCliente();
+        
+        painelPrincipal.add(cadCli, "CadastroCliente");  
+        painelPrincipal.add(pRosa, "Painel Rosa");
+        painelPrincipal.add(pAmarelo, "Painel Amarelo");
+        
+        this.setExtendedState(MAXIMIZED_BOTH);// inicia a tela maximizada.
+        
     }
 
     /**
@@ -32,24 +47,19 @@ public class SisLojFrame extends javax.swing.JFrame {
         painelPrincipal = new javax.swing.JPanel();
         barradeMenu = new javax.swing.JMenuBar();
         menuCliente = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
+        cadastrarCliente = new javax.swing.JMenuItem();
+        listarCliente = new javax.swing.JMenuItem();
         menuProduto = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
+        paineisColorido = new javax.swing.JMenu();
+        painelRosa = new javax.swing.JMenuItem();
+        painelAmarelo = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setSize(new java.awt.Dimension(600, 450));
 
-        javax.swing.GroupLayout painelPrincipalLayout = new javax.swing.GroupLayout(painelPrincipal);
-        painelPrincipal.setLayout(painelPrincipalLayout);
-        painelPrincipalLayout.setHorizontalGroup(
-            painelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        painelPrincipalLayout.setVerticalGroup(
-            painelPrincipalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 279, Short.MAX_VALUE)
-        );
+        painelPrincipal.setLayout(new java.awt.CardLayout());
 
         menuCliente.setText("Cliente");
         menuCliente.addActionListener(new java.awt.event.ActionListener() {
@@ -58,23 +68,23 @@ public class SisLojFrame extends javax.swing.JFrame {
             }
         });
 
-        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F6, 0));
-        jMenuItem1.setText("Cadastrar cliente");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        cadastrarCliente.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F6, 0));
+        cadastrarCliente.setText("Cadastrar cliente");
+        cadastrarCliente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                cadastrarClienteActionPerformed(evt);
             }
         });
-        menuCliente.add(jMenuItem1);
+        menuCliente.add(cadastrarCliente);
 
-        jMenuItem3.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F7, 0));
-        jMenuItem3.setText("Listar cliente");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+        listarCliente.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F7, 0));
+        listarCliente.setText("Listar cliente");
+        listarCliente.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
+                listarClienteActionPerformed(evt);
             }
         });
-        menuCliente.add(jMenuItem3);
+        menuCliente.add(listarCliente);
 
         barradeMenu.add(menuCliente);
 
@@ -87,6 +97,28 @@ public class SisLojFrame extends javax.swing.JFrame {
         menuProduto.add(jMenuItem4);
 
         barradeMenu.add(menuProduto);
+
+        paineisColorido.setText("Paineis Coloridos");
+
+        painelRosa.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F8, 0));
+        painelRosa.setText("Painel Rosa");
+        painelRosa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                painelRosaActionPerformed(evt);
+            }
+        });
+        paineisColorido.add(painelRosa);
+
+        painelAmarelo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F9, 0));
+        painelAmarelo.setText("Painel Amarelo");
+        painelAmarelo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                painelAmareloActionPerformed(evt);
+            }
+        });
+        paineisColorido.add(painelAmarelo);
+
+        barradeMenu.add(paineisColorido);
 
         setJMenuBar(barradeMenu);
 
@@ -109,15 +141,28 @@ public class SisLojFrame extends javax.swing.JFrame {
         JOptionPane.showMessageDialog(null, "teste com atalho");
     }//GEN-LAST:event_menuClienteActionPerformed
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        // TODO add your handling code here:
-        JOptionPane.showMessageDialog(null, "teste com atalho");
-    }//GEN-LAST:event_jMenuItem1ActionPerformed
+    private void cadastrarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastrarClienteActionPerformed
+       CardLayout cl = (CardLayout) painelPrincipal.getLayout();
+        cl.show(painelPrincipal, "CadastroCliente");
+       
+    }//GEN-LAST:event_cadastrarClienteActionPerformed
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+    private void listarClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_listarClienteActionPerformed
         // TODO add your handling code here:
         JOptionPane.showMessageDialog(null, "teste com atalho 2....");
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
+    }//GEN-LAST:event_listarClienteActionPerformed
+
+    private void painelRosaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_painelRosaActionPerformed
+        // TODO add your handling code here:
+        CardLayout cl = (CardLayout) painelPrincipal.getLayout();
+        cl.show(painelPrincipal, "Painel Rosa");
+    }//GEN-LAST:event_painelRosaActionPerformed
+
+    private void painelAmareloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_painelAmareloActionPerformed
+        // TODO add your handling code here:
+        CardLayout cl = (CardLayout) painelPrincipal.getLayout();
+        cl.show(painelPrincipal, "Painel Amarelo");
+    }//GEN-LAST:event_painelAmareloActionPerformed
 
     /**
      * @param args the command line arguments
@@ -156,12 +201,15 @@ public class SisLojFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuBar barradeMenu;
-    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem cadastrarCliente;
     private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem listarCliente;
     private javax.swing.JMenu menuCliente;
     private javax.swing.JMenu menuProduto;
+    private javax.swing.JMenu paineisColorido;
+    private javax.swing.JMenuItem painelAmarelo;
     private javax.swing.JPanel painelPrincipal;
+    private javax.swing.JMenuItem painelRosa;
     // End of variables declaration//GEN-END:variables
 }
